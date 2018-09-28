ee4657544a3d7ebd9f85625ac2b7d7de17bc3a5c
Rana-Fahad-Altaf <kingfahad103@gmail.com>
2018-09-28 21:16:36 +0500

    profile segments added n page complete

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
 /**
  * Generated class for the ProfilePage page.
  *
@@ -14,8 +13,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
+  profileSegment: any;
   items: any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.profileSegment = 'products',
     this.items = [
       {Name:'Capsicum',Phone:'Product Description',Followers:'100 Rs',Category:'Vegetable',ImagePath:'assets/imgs/vegetable.png'},
       {Name:'Capsicum',Phone:'Product Description',Followers:'100 Rs',Category:'Vegetable',ImagePath:'assets/imgs/vegetable.png'},
